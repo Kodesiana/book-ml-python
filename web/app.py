b52a7b040abd9682a4bfda2080677d981a5bd106
fahminlb33 <fahminlb33@gmail.com>
2022-03-20 12:00:21 +0700

    Update API path to use new API server

--- web/app.py
+++ web/app.py
@@ -13,21 +13,22 @@ app = Flask(__name__)
 # --- API routes
 @app.route("/predict-house", methods=["POST"])
 def predict_house():
-    body = { 'rm': request.form["ruangan"] }
-    result = requests.post("http://localhost:3001/predict", data=body)
+    body = { 'rm': request.form["rm"], 'lstat': request.form["lstat"] }
 
+    result = requests.post("http://localhost:5000/predict_reg", data=body)
     response = result.json()
-    return render_template('hasil.html', hasil=response["predicted"])
     
+    body["hasil"] = response["predicted"]
+    return render_template('hasil.html', **body)
 
-@app.route("/predict-animals", methods=["POST"])
-def predict_animals():
+@app.route("/predict-cifar", methods=["POST"])
+def predict_cifar():
     file = request.files["file"]
     save_path = os.path.join(tempfile.gettempdir(), file.filename)
     file.save(save_path)
 
     body = { 'file': ('image.jpg', open(save_path, 'rb')) }
-    result = requests.post("http://localhost:3002/predict", files=body)
+    result = requests.post("http://localhost:5000/predict_classify", files=body)
 
     with open(save_path, "rb") as img_file:
         image_data = "data:image/jpg;base64," + base64.b64encode(img_file.read()).decode('utf-8')
@@ -42,4 +43,4 @@ def index():
 
 # --- App entry point
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
